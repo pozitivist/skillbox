e1afb943c25f7888a339adcec337899e10c8a672
pozitivist <alt_anest@mail.ru>
2019-05-24 11:32:40 +0300

    HM_12

--- hm_12_result.php
+++ hm_12_result.php
@@ -1,50 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Home Work #12</title>
-</head>
-<body>
-<h1>Данные нового пользователя</h1>
-
-<table>
 <?php
-foreach ($_POST as $key => $value) {
-	switch($key){
-		case "name":$key="Имя";break;
-		case "sname":$key="Фамилия";break;
-		case "date":$key="Дата рождения";break;
-		case "address":$key="Адрес";break;
-		case "tel":$key="Тел.";break;
-		case "mail":$key="Email";break;
-	}
 
-	if ($value=="") {$value='<span style="color:#f00;">Нет данных.</span>';}
-	echo "<tr><td>".$key.":</td><td>".$value."</td></tr>";
-}
+//echo '<h1>Данные пользователя:</h1>';
+
+header("Content-Type: image/png");
+$name=name;
+$sname=sname;
+$date=date;
+$address=address;
+$tel=tel;
+$mail=mail;
+
+$im=imagecreate(200,200);
+$bg_color=imagecolorallocate($im, 0, 0, 0);
+$txt_color=imagecolorallocate($im, 255, 255, 255);
+imagestring($im, 5, 50, 50, $name, $txt_color);
+imagestring($im, 5, 50, 70, $sname, $txt_color);
+imagestring($im, 5, 50, 90, $date, $txt_color);
+imagestring($im, 5, 50, 110, $address, $txt_color);
+imagestring($im, 5, 50, 130, $tel, $txt_color);
+imagestring($im, 5, 50, 150, $mail, $txt_color);
+
+imagepng($im);
+imagedestroy($im);
 ?>
-</table>
-
-<?php
-function result(){
-
-	foreach ($_POST as $key => $value) {
-		switch($key){
-			case "name":$key="Имя";break;
-			case "sname":$key="Фамилия";break;
-			case "date":$key="Дата рождения";break;
-			case "address":$key="Адрес";break;
-			case "tel":$key="Тел.";break;
-			case "mail":$key="Email";break;
-		}
-
-		if ($value=="") {$value='<span style="color:#f00;">Нет данных.</span>';}
-		echo "<tr><td>".$key.":</td><td>".$value."</td></tr>";
-	}
-?>
-<table><?php result();?></table>
-<br>
-
-<img src="result_img.php">
-
-</body>
-</html>
