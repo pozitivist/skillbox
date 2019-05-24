c1e235862df4359c3acd8b9f4f602b9b08b4eee3
pozitivist <alt_anest@mail.ru>
2019-05-24 11:59:25 +0300

    HM_12 Done

--- hm_12_result.php
+++ hm_12_result.php
@@ -3,16 +3,16 @@
 //echo '<h1>Данные пользователя:</h1>';
 
 header("Content-Type: image/png");
-$name=name;
-$sname=sname;
-$date=date;
-$address=address;
-$tel=tel;
-$mail=mail;
+$name = $_POST['name'];
+$sname = $_POST['sname'];
+$date = $_POST['date'];
+$address = $_POST['address'];
+$tel = $_POST['tel'];
+$mail = $_POST['mail'];
 
-$im=imagecreate(200,200);
-$bg_color=imagecolorallocate($im, 0, 0, 0);
-$txt_color=imagecolorallocate($im, 255, 255, 255);
+$im = imagecreate(200,200);
+$bg_color = imagecolorallocate($im, 0, 0, 0);
+$txt_color = imagecolorallocate($im, 255, 255, 255);
 imagestring($im, 5, 50, 50, $name, $txt_color);
 imagestring($im, 5, 50, 70, $sname, $txt_color);
 imagestring($im, 5, 50, 90, $date, $txt_color);
